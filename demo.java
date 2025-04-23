cb80fdbc87f5e815740cb287e7b5496e1d72e90c
NikhilShetty04 <nikhilshetty04@gamil.com>
2025-04-23 14:37:32 +0530

    save java file

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo
+{
+    public static void main(String[] args) 
+    {
+        int a=10,b=5;
+
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction"+(a-b));
+
+    }
+}
